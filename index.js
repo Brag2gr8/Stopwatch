42a0490f9ef3a1dd3e18729342099301b9ed079c
Brag2gr8 <58727101+Brag2gr8@users.noreply.github.com>
2022-12-02 14:20:46 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+
+let [seconds,minutes,hours] = [0,0,0];
+let timerRef = document.querySelector('.timerDisplay');
+let int = null;
+let bit = null;
+
+const hourEl = document.querySelector(".hours");
+const minuteEl = document.querySelector(".minutes");
+const secondEl = document.querySelector(".seconds");
+
+const borderEl = document.querySelector(".wrapper");
+
+
+document.getElementById("start").addEventListener("click", () => {
+    borderEl.style.borderColor = "greenyellow";
+    if(int!==null){
+        clearInterval(int);
+    };
+    int = setInterval(displayTimer,1000);
+    
+    bit = setInterval(toggleBorder,2000);
+});
+
+function toggleBorder() {
+    if(borderEl.style.borderColor = "greenyellow"){
+        borderEl.style.borderColor = "white"
+    }
+    setTimeout(() => {if (borderEl.style.borderColor = "white") {
+        borderEl.style.borderColor = "greenyellow"
+    }}, 1000)
+    
+}
+
+document.getElementById('pause').addEventListener('click', () => {
+    clearInterval(bit);
+    if (secondEl.innerHTML > 0) {
+        clearInterval(int);
+        setTimeout(() => borderEl.style.borderColor = "orange", 500)
+    } else { alert("NO ACTIVE TIME")}
+});
+
+document.getElementById("stop").addEventListener("click", () => {
+    if (secondEl.innerHTML > 0) {
+        document.getElementById("time-count").textContent = `${hourEl.textContent} 
+        : ${minuteEl.textContent} : ${secondEl.textContent} `
+        clearInterval(int);
+        [seconds,minutes,hours] = [0,0,0,0];
+        hourEl.textContent = '00';
+        minuteEl.textContent = '00';
+        secondEl.textContent = '00';
+        clearInterval(bit);
+        setTimeout(() => borderEl.style.borderColor = "red", 500);
+    } else { alert("NO ACTIVE TIME")}
+})
+
+
+function displayTimer(){
+    seconds+=1;
+    
+    if(seconds == 60){
+        seconds = 0;
+        minutes++;
+        if(minutes == 60){
+            minutes = 0;
+            hours++;
+        }
+    }  
+     
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+    
+    hourEl.innerHTML = h;
+    minuteEl.innerHTML = m;
+    secondEl.innerHTML = s;
+}
